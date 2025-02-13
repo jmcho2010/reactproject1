86039617f296d506b62f2738b0d137182b33be6b
jmcho2010 <projm2786@gmail.com>
2025-02-13 15:55:55 +0900

    test

--- project1/src/chatUI/App.js
+++ project1/src/chatUI/App.js
@@ -3,7 +3,7 @@
 // npm i
 // npm i --save react-typing-animation
 import React, {useState, useEffect} from 'react';
-import Typing from 'react-typing-animation';
+//import Typing from 'react-typing-animation';
 
 const App = () =>{
     
@@ -28,18 +28,70 @@ const App = () =>{
     //     ...prevMessages,
     //     {어떠한 값들을 추가하는 업데이트 진행}
     // ]);
+    // 파라미터 message는 MessageForm으로 부터 전달 받아옴
+    //  -> 일반 유저가 입력한 문자열
+    const handleSendMessage = (message) => {
+        // 받아온 메세지의 상태를 업데이트.(함수형 업데이트)
+        //  -> 함수형 업데이트를 반드시써야하는가?
+        //  1. 상태에 대한 action이 비동기적으로 처리될수 있어서
+        //     (데이터 처리가 비동기로 될수 있기 때문)
+        //  2. 함수가 실행되는 동안 최신화가 안될수 있어서.
+        setMessages((prevMessages) =>[
+            ...prevMessages, //이전 메세지 기억 -> 스프레드 연산자를 사용하여
+                             // 새로운 배열을 생성한후 기존에 저장했던 메세지들을 담아둠
+                             // react의 객체들이 기본적으로 불변성을 띄기때문에.
+            {text: message, isUser: true}, // 유저가 입력한 내용이 출력
+            {text: `당신이 입력한 메세지는 : "${message}"`, isUser:false}//ai내용출력
+        ]);
+    };
+
 
     return(
-        <div>
-            <div>
-                
+        <div className='app'>
+            <div className='chat-box'>
+                <h1>Chat App</h1>
+                {/*컴포넌트 영역 MessageList, MessageForm
+                MessageList에서는 메세지의 목록을 조회
+                MessageForm에서는 데이터 입력과 메세지 관리.
+                */ }
             </div>
-            <div>
-                
-            </div>
-        <form>
-            <input></input>
-        </form>
+
         </div>
-    );
+    )
+
+
+
 };
+
+const MessageForm = ({ onSendMessage }) =>{
+
+    // MessageForm에서는 어차피 넘겨야할 데이터는 문자열
+    //  -> 문자열 : 사용자가 입력한 값 자체를 의미.
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        onSendMessage(message);
+        setMessage('');
+    }
+
+
+    return(
+        <form className='message-form' onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                className="message-input"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className="send-button" type="submit">
+                Send
+            </button>
+        </form>
+      
+    );
+
+}
+
+
+export default App;
